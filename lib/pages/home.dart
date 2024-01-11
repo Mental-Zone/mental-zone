d8bfe19814ec0c465bee364cae09c2b2a8470b6b
ScaferuZ <endrafruz@gmail.com>
2024-01-12 00:48:03 +0700

    feat: creating moodboard section

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
@@ -15,6 +16,7 @@ class HomePage extends StatelessWidget {
           Container(
             padding: EdgeInsets.symmetric(horizontal: 25, vertical: 20),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -65,7 +67,127 @@ class HomePage extends StatelessWidget {
                       child: Icon(Icons.notifications),
                     ),
                   ],
-                )
+                ),
+                const Gap(37),
+                Container(
+                    padding: EdgeInsets.symmetric(vertical: 18),
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black.withOpacity(0.08),
+                              blurRadius: 15,
+                              offset: Offset(0, 2))
+                        ]),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Bagaimana Perasaanmu hari ini?',
+                          style: GoogleFonts.montserrat(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black),
+                        ),
+                        const Gap(18),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Container(
+                                width: 48,
+                                height: 48,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(8),
+                                  boxShadow: [
+                                    BoxShadow(
+                                        color: Colors.black.withOpacity(0.08),
+                                        blurRadius: 15,
+                                        offset: Offset(0, 2))
+                                  ],
+                                  border: Border.all(
+                                      color: Color(0xFFCCF1F8), width: 1),
+                                ),
+                                child: IconButton(
+                                  icon: Image.asset('assets/img/angy.png'),
+                                  onPressed: () {},
+                                )),
+                            Container(
+                                width: 48,
+                                height: 48,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(8),
+                                  boxShadow: [
+                                    BoxShadow(
+                                        color: Colors.black.withOpacity(0.08),
+                                        blurRadius: 15,
+                                        offset: Offset(0, 2))
+                                  ],
+                                  border: Border.all(
+                                      color: Color(0xFFCCF1F8), width: 1),
+                                ),
+                                child: IconButton(
+                                  icon: Image.asset('assets/img/sad.png'),
+                                  onPressed: () {},
+                                )),
+                            Container(
+                                width: 48,
+                                height: 48,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(8),
+                                  boxShadow: [
+                                    BoxShadow(
+                                        color: Colors.black.withOpacity(0.08),
+                                        blurRadius: 15,
+                                        offset: Offset(0, 2))
+                                  ],
+                                  border: Border.all(
+                                      color: Color(0xFFCCF1F8), width: 1),
+                                ),
+                                child: IconButton(
+                                  icon: Image.asset('assets/img/confused.png'),
+                                  onPressed: () {},
+                                )),
+                            Container(
+                                width: 48,
+                                height: 48,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(8),
+                                  boxShadow: [
+                                    BoxShadow(
+                                        color: Colors.black.withOpacity(0.08),
+                                        blurRadius: 15,
+                                        offset: Offset(0, 2))
+                                  ],
+                                  border: Border.all(
+                                      color: Color(0xFFCCF1F8), width: 1),
+                                ),
+                                child: IconButton(
+                                  icon: Image.asset('assets/img/hepi.png'),
+                                  onPressed: () {},
+                                )),
+                            Container(
+                                width: 48,
+                                height: 48,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(8),
+                                  boxShadow: [
+                                    BoxShadow(
+                                        color: Colors.black.withOpacity(0.08),
+                                        blurRadius: 15,
+                                        offset: Offset(0, 2))
+                                  ],
+                                  border: Border.all(
+                                      color: Color(0xFFCCF1F8), width: 1),
+                                ),
+                                child: IconButton(
+                                  icon: Image.asset('assets/img/lovely.png'),
+                                  onPressed: () {},
+                                )),
+                          ],
+                        )
+                      ],
+                    ))
               ],
             ),
           )
