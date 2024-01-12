32c2a31160c8b7ac27635131cb7a407ffd826027
ScaferuZ <endrafruz@gmail.com>
2024-01-12 14:51:17 +0700

    theme: adding TextStyle contstants

---
+++ lib/shared/theme/font.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle sectionHeader = GoogleFonts.montserrat(
+  fontSize: 14,
+  fontWeight: FontWeight.w600,
+  color: Colors.black,
+);
+
+TextStyle paragraph = GoogleFonts.montserrat(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+  color: Colors.black,
+);
