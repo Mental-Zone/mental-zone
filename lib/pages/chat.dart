3ebbd5940dcbad47207972618b9d7864ef7f08f3
ScaferuZ <endrafruz@gmail.com>
2024-01-12 22:09:33 +0700

    feat: chatbot feature //need to work on the AI

--- lib/pages/chat.dart
+++ lib/pages/chat.dart
@@ -1,4 +1,12 @@
+// ignore_for_file: prefer_const_declarations, prefer_const_constructors
+
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mental_zone/shared/theme/color.dart';
+import 'package:http/http.dart' as http;
 
 class ChatPage extends StatefulWidget {
   static const routeName = '/menzai';
@@ -9,11 +17,214 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  final TextEditingController _chatController = TextEditingController();
+  final ScrollController _scrollController = ScrollController();
+  List<Map<String, dynamic>> _chatHistory = [];
+
+  void getAnswer() async {
+    final url =
+        "https://generativelanguage.googleapis.com/v1beta2/models/chat-bison-001:generateMessage?key=AIzaSyD-gdqEyig7pVwg5jwXd02nL9Yu2PYn2Iw";
+    final uri = Uri.parse(url);
+    List<Map<String, String>> msg = [];
+    for (var i = 0; i < _chatHistory.length; i++) {
+      msg.add({"content": _chatHistory[i]["message"]});
+    }
+
+    Map<String, dynamic> request = {
+      "prompt": {
+        "messages": [msg]
+      },
+      "temperature": 0.25,
+      "candidateCount": 1,
+      "topP": 1,
+      "topK": 1
+    };
+
+    final response = await http.post(uri, body: jsonEncode(request));
+
+    setState(() {
+      _chatHistory.add({
+        "time": DateTime.now(),
+        "message": json.decode(response.body)["candidates"][0]["content"],
+        "isSender": false,
+      });
+    });
+
+    _scrollController.jumpTo(
+      _scrollController.position.maxScrollExtent,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Chat Page'),
+    return Scaffold(
+        appBar: _appBar(),
+        body: Stack(
+          children: [
+            Container(
+              color: Colors.white,
+              height: MediaQuery.of(context).size.height - 160,
+              child: ListView.builder(
+                  shrinkWrap: false,
+                  itemCount: _chatHistory.length,
+                  controller: _scrollController,
+                  padding: const EdgeInsets.only(top: 10, bottom: 10),
+                  physics: const BouncingScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    return Container(
+                      padding: EdgeInsets.only(
+                          left: 14, right: 14, top: 10, bottom: 10),
+                      child: Align(
+                        alignment: (_chatHistory[index]["isSender"]
+                            ? Alignment.topRight
+                            : Alignment.topLeft),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: _chatHistory[index]["isSender"]
+                                ? BorderRadius.only(
+                                    topLeft: Radius.circular(20),
+                                    bottomLeft: Radius.circular(20),
+                                    bottomRight: Radius.circular(20))
+                                : BorderRadius.only(
+                                    topRight: Radius.circular(20),
+                                    bottomLeft: Radius.circular(20),
+                                    bottomRight: Radius.circular(20)),
+                            color: (_chatHistory[index]["isSender"]
+                                ? primaryColor
+                                : Color(0xFFF5F5F5)),
+                          ),
+                          padding: EdgeInsets.all(16),
+                          child: Text(_chatHistory[index]["message"],
+                              style: GoogleFonts.montserrat(
+                                  fontSize: 12,
+                                  height: 1.8,
+                                  fontWeight: FontWeight.w400,
+                                  color: _chatHistory[index]["isSender"]
+                                      ? Colors.white
+                                      : Color(0xFF263238))),
+                        ),
+                      ),
+                    );
+                  }),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                // padding:
+                //     const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+                margin: const EdgeInsets.only(bottom: 35, right: 40, left: 23),
+                // height: 60,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(16),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color(0xFF263238).withOpacity(0.1),
+                      blurRadius: 15,
+                      offset: const Offset(0, 8),
+                    ),
+                  ],
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: TextField(
+                          style: GoogleFonts.montserrat(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.black),
+                          decoration: InputDecoration(
+                            hintText: "Ketik sesuatu",
+                            hintStyle: GoogleFonts.montserrat(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.black),
+                            border: InputBorder.none,
+                            contentPadding: EdgeInsets.all(8.0),
+                          ),
+                          controller: _chatController,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 4.0,
+                    ),
+                    MaterialButton(
+                      onPressed: () {
+                        setState(() {
+                          if (_chatController.text.isNotEmpty) {
+                            _chatHistory.add({
+                              "time": DateTime.now(),
+                              "message": _chatController.text,
+                              "isSender": true
+                            });
+                            _chatController.clear();
+                          }
+                        });
+                        _scrollController
+                            .jumpTo(_scrollController.position.maxScrollExtent);
+
+                        getAnswer();
+                      },
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(80.0)),
+                      padding: const EdgeInsets.all(0.0),
+                      child: Icon(
+                        Icons.send,
+                        color: Colors.black,
+                        size: 20,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ));
+  }
+
+  AppBar _appBar() {
+    return AppBar(
+      title: Row(
+        children: [
+          Container(
+            margin: EdgeInsets.only(right: 17),
+            width: 40,
+            height: 40,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: primaryColor,
+                image: DecorationImage(
+                  image: AssetImage('assets/img/robot.png'),
+                  alignment: Alignment.bottomCenter,
+                )),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Menzai',
+                style: GoogleFonts.montserrat(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black,
+                ),
+              ),
+              const Gap(5),
+              Text(
+                'Online',
+                style: GoogleFonts.montserrat(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.black),
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
