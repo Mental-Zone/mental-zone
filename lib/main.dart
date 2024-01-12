7b0394b2115b1229df14d1016f2be92852b21064
firstkiadityaf12 <adityafernanda2013@gmail.com>
2024-01-12 17:41:02 +0700

    Commit message describing your changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mental_zone/pages/onboarding_controller.dart';
 import 'package:mental_zone/pages/home.dart';
 
 void main() {
@@ -17,7 +18,8 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        initialRoute: HomePage.routeName,
+        // initialRoute: HomePage.routeName,
+        home: OnBoardingScreen(),
         routes: {
           HomePage.routeName: (context) => const HomePage(),
         });
