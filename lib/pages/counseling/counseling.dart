eff76297bbc2835e22e78152dea4033ee0037d90
ScaferuZ <endrafruz@gmail.com>
2024-01-15 15:38:17 +0700

    feat: doctor container done

--- lib/pages/counseling/counseling.dart
+++ lib/pages/counseling/counseling.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:iconly/iconly.dart';
 import 'package:mental_zone/shared/theme/color.dart';
 import 'package:mental_zone/shared/theme/font.dart';
 import 'package:mental_zone/utils/app_styles.dart';
@@ -39,14 +40,116 @@ class _CounselingPageState extends State<CounselingPage> {
                         "Konsultasi online dengan ahli!",
                         style: sectionHeader,
                       ),
+                      const Gap(10),
                       Text(
                         'Temukan dan pilih konselor yang kamu rasa sesuai!',
                         style: paragraph,
-                      )
+                      ),
+                      const Gap(37),
+                      _doctorContainer(),
                     ],
                   )
                 ],
               ))
+        ],
+      ),
+    );
+  }
+
+  Container _doctorContainer() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+      decoration: BoxDecoration(boxShadow: [
+        BoxShadow(
+          color: Colors.black.withOpacity(0.05),
+          blurRadius: 15,
+          offset: Offset(0, 9),
+        )
+      ], color: Colors.white, borderRadius: BorderRadius.circular(15)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Image.asset(
+              'assets/img/counseling/doktor1.png',
+              width: 92,
+              height: 89,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const Gap(19),
+          Flexible(
+            fit: FlexFit.loose,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Chrysti Denie, S.Psi',
+                      style: sectionHeader,
+                    ),
+                    // IconButton(
+                    //   icon: Icon(IconlyBold.heart),
+                    //   color: primaryColor,
+                    //   onPressed: () {},
+                    //   iconSize: 20,
+                    // )
+                    Icon(
+                      IconlyBold.heart,
+                      color: primaryColor,
+                      size: 20,
+                    )
+                  ],
+                ),
+                Divider(
+                  color: Colors.black.withOpacity(0.08),
+                  thickness: 0.4,
+                ),
+                Row(
+                  children: [
+                    Text(
+                      'Expertise :',
+                      style: GoogleFonts.montserrat(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                          color: primaryColor),
+                    ),
+                    Text(
+                      ' Kendali Emosi',
+                      style: GoogleFonts.montserrat(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.black.withOpacity(0.8)),
+                    )
+                  ],
+                ),
+                const Gap(6),
+                Row(
+                  children: [
+                    Icon(
+                      IconlyBold.star,
+                      color: primaryColor,
+                      size: 16,
+                    ),
+                    const Gap(8),
+                    Text(
+                      '4.8 (60 Reviews)',
+                      style: GoogleFonts.montserrat(
+                          fontSize: 12,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.black),
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
